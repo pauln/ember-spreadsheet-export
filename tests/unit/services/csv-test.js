fa9567c5326e2a815d080d2017dcb7536ebdd67f
John Ryan <john.ryan@cardinalhealth.com>
2019-02-12 14:05:08 -0500

    Failing test to demonstrate the missing time of day in the results.

--- tests/unit/services/csv-test.js
+++ tests/unit/services/csv-test.js
@@ -1,11 +1,12 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import {module, test} from 'qunit';
+import {setupTest} from 'ember-qunit';
+import moment from 'moment';
 
-module('Unit | Service | csv', function(hooks) {
+module('Unit | Service | csv', function (hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('#jsonToCsv - with valid Array of Arrays, it should return an string with CSV format', function(assert) {
+  test('#jsonToCsv - with valid Array of Arrays, it should return an string with CSV format', function (assert) {
     const service = this.owner.lookup('service:csv');
     const array = [
       [
@@ -27,7 +28,28 @@ module('Unit | Service | csv', function(hooks) {
     assert.deepEqual(service.jsonToCsv(array, options), expectedCSV);
   });
 
-  test('#jsonToCsv - with raw mode on, it should return a string in CSV format without quote wrapping', function(assert) {
+  test('#jsonToCsv - it should provide time of day information with date fields', function (assert) {
+    const service = this.owner.lookup('service:csv');
+    const fakeDateWithTime = '2019-01-01 14:15:16';
+    const array = [
+      [
+        'test_date',
+      ],
+      [
+        moment(fakeDateWithTime)
+      ],
+    ];
+    const expectedCSV = '"test_date"\r\n"' + fakeDateWithTime + '"\r\n';
+    const options = {
+      separator: ',',
+      withSeparator: false,
+      raw: false,
+    };
+
+    assert.deepEqual(service.jsonToCsv(array, options), expectedCSV);
+  });
+
+  test('#jsonToCsv - with raw mode on, it should return a string in CSV format without quote wrapping', function (assert) {
     const service = this.owner.lookup('service:csv');
     const array = [
       [
