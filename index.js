c143184cbd624b04d8791e38765865304d036c3b
Eric Hubbell <eric.hubbell@gmail.com>
2019-04-01 14:58:50 -0600

    wrap addon in fastboot check

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 'use strict';
 
-module.exports = {
-  name: 'ember-spreadsheet-export',
+var path = require('path');
+var MergeTrees = require('broccoli-merge-trees');
+var Funnel = require('broccoli-funnel');
+var map = require('broccoli-stew').map;
 
-  included: function(app) {
-    this._super.included(app);
+module.exports = {
+  name: 'ember-cli-spreadsheet-export',
+
+	treeForVendor(defaultTree) {
+ 		var browserVendorLib = new Funnel(path.join(this.project.root, 'vendor'), { files: ['Blob.js', 'FileSaver-1.3.3.js', 'jszip-0.10.8.js', 'xlsx-0.10.8.js'] } );
+    
+    browserVendorLib = map(browserVendorLib, (content) => `if (typeof FastBoot === 'undefined') { ${content} }` );
+    
+    return new MergeTrees([defaultTree, browserVendorLib], { overwrite: true });
+	},
+
+  included(app) {
+  	this._super.included(app);
     app.import('vendor/Blob.js');
-    app.import('vendor/FileSaver-1.3.3.js');
-    app.import('vendor/jszip-0.10.8.js');
+		app.import('vendor/FileSaver-1.3.3.js');
+		app.import('vendor/jszip-0.10.8.js');
     app.import('vendor/xlsx-0.10.8.js');
   }
 };
